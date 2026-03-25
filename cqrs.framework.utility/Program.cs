33a369e860f8be19d8b0a5e1dfcce99e0307c715
Matt Ashurst <mashurst99@gmail.com>
2026-03-25 02:13:35 -0500

    Added a test utility to demonstrate and test new commands/queries

---
+++ cqrs.framework.utility/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using cqrs.framework.Dispatchers;
+using cqrs.framework.Interfaces;
+using cqrs.framework.Queries.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+//Setup DI
+var services = new ServiceCollection();
+
+services.AddTransient<IQueryHandler<GetUserById, GetUserByIdResult>>();
+services.AddSingleton<IQueryDispatcher, QueryDispatcher>();
+
+var provider = services.BuildServiceProvider();
+
+//Get dispatcher
+var queryDispatcher = provider.GetRequiredService<IQueryDispatcher>();
+
+//Execute a query
+var result = await queryDispatcher.DispatchAsync<GetUserById, GetUserByIdResult>(new GetUserById
+{
+    Id = 1
+});
+
+Console.WriteLine(result.FirstName);
